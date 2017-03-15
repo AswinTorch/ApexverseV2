c6686cf7a385f2daf8ea2a2a0dd8f01f6a90a636
Aswin Nair <aswintorch@gmail.com>
2017-03-15 12:58:16 -0400

    Added mailer stuff

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'app65126754@heroku.com',
+  :password => 'recrdiwy5269',
+  :domain => 'testapex.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
