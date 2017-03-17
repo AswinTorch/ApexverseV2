166a3cd607773b97668d571c8113a3dd245008d4
Aswin Nair <aswintorch@gmail.com>
2017-03-17 15:03:15 -0400

    Added store view and changed w3-btn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'store', to: 'pages#store'
   resources :contacts, only: :create
   get 'contact-us', to: 'contacts#new', as: 'new_contact'
   resources :articles
